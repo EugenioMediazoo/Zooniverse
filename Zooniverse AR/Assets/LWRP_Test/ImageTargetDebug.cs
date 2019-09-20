7f20160f89e4a4fc5f64d7d096e65f7dc13348bc
EugenioMediazoo <eugenio@mediazoo.tv>
2019-09-20 16:21:24 +0100

    [~] debugger

---
+++ Zooniverse AR/Assets/LWRP_Test/ImageTargetDebug.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetDebug : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    //public Debug TEXT;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            Debug.Log("gotcha");
+        }
+        else
+        {
+            // Stop audio when target is lost
+            Debug.Log("Where are you?");
+        }
+    }
+}
