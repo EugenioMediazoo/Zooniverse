4f65db9dacb9471bf4f05626aba32c0b02fe71d5
EugenioMediazoo <eugenio@mediazoo.tv>
2019-09-23 13:20:27 +0100

    [+] debug log

---
+++ Zooniverse AR/Assets/LWRP_Test/ButtonDebug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void PrintNow()
+    {
+        Debug.Log("test");
+        Debug.LogWarning("testWarning");
+        Debug.LogError("testError");
+    }
+}
